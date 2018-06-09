ea525f1f403a9a061d7e5082ceebef259f8e2cf0
ty <towyuan@outlook.com>
2018-06-09 23:40:51 +0800

    export utils

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
-import {RPC} from "./rpc";
-
 export * from "./defines";
 export * from "./errors";
+export * from "./utils";
 export * from "./transport";
 export * from "./transport.stream";
 export * from "./framers";
 export * from "./provider";
 export * from "./rpc";
-
-export const createRPC = RPC.create;
